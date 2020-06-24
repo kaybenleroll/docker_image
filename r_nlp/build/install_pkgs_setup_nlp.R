cbbbf461ba62190be9685870fc640435db179116
Mick Cooney <mcooney@describedata.com>
2020-06-24 14:13:15 +0000

    Tweaking Makefiles and constructing the NLP image

---
+++ r_nlp/build/install_pkgs_setup_nlp.R
@@ -0,0 +1,17 @@
+remotes::install_github(
+  "pommedeterresautee/fastrtext",
+  ref = "b63c5de9a5168378e8e1abfc4a50be7292002bfb",
+  upgrade = "never"
+)
+
+install.packages("openNLPmodels.en",
+  dependencies = TRUE,
+  repos = "http://datacube.wu.ac.at/"
+)
+
+
+library(cleanNLP)
+
+cnlp_download_spacy()
+cnlp_download_corenlp()
+cnlp_init_udpipe()
